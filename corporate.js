b065968ae0e15659e20a08615ddd9e29674ae628
Big-dish <91467606+Big-dish@users.noreply.github.com>
2021-10-19 21:56:22 +0900

    test

---
+++ corporate.js
@@ -0,0 +1,4 @@
+btn.addEventListener('click',function() {
+    //click
+    let newwin=window.open('file:///C:/Users/0975/Desktop/11%E6%9C%88%E5%88%86%E5%80%AB%E7%90%86%E7%A0%94%E4%BF%AE/source%20question/index.html');
+});
